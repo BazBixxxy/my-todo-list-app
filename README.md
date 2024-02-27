62f834aeea171aee86c552882ca59737c1f240eb
BazBixxxy <133676922+BazBixxxy@users.noreply.github.com>
2024-02-27 15:08:11 +0300

    first version of my to-do app, readme file has issues that need attending

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # my-todo-list-app
 A simple app to manage and store your daily task [to-do(s)]
+
+
+Improvements:
+> use date-fns to automatically delete an item when its times up
+
+> add effects to when an item has been deleted
+
+> add local storage to the application
+
+
+
+Issues:
+> local storage is not functional
+
+> issues with my output management in my webpack
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import "./general.css";
+import "./header.css";
+import "./sidebar.css";
+import "./form.css";
+import "./style.css";
+import "./card.css";
+import "@fortawesome/fontawesome-free/js/fontawesome";
+import "@fortawesome/fontawesome-free/js/solid";
+import "@fortawesome/fontawesome-free/js/regular";
+import "@fortawesome/fontawesome-free/js/brands";
+// import "./script.js"
