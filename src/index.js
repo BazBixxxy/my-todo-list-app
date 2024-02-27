7f5ee79ffd2a7857ef2c611dca225723084ae0e2
BazBixxxy <133676922+BazBixxxy@users.noreply.github.com>
2024-02-27 21:47:04 +0300

    test fixing the form to add items to the list

--- src/index.js
+++ src/index.js
@@ -8,4 +8,4 @@ import "@fortawesome/fontawesome-free/js/fontawesome";
 import "@fortawesome/fontawesome-free/js/solid";
 import "@fortawesome/fontawesome-free/js/regular";
 import "@fortawesome/fontawesome-free/js/brands";
-import "./script.js";
+// import "./script.js";
